0720801544daf37553d67ea6059c92379a8097d6
Theo Janke-Furman <tjankefurman@gmail.com>
2022-10-26 15:47:17 -0500

    Migrations added

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -3,7 +3,7 @@ using System;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-namespace pet_hotel
+namespace pet_hotel.Models
 {
     public class PetOwner {
 
